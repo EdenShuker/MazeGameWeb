fa87b5e7b048aa1bf4069c300b8cce43a0edcae2
Eden Shuker <edenshuker1997@gmail.com>
2017-06-10 21:07:14 +0300

    updates

---
+++ MazeGame/Controllers/GenerateMazeController.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MazeLib;
+
+
+namespace MazeGame.Controllers
+{
+    public class GenerateMazeController : ApiController
+    {
+
+    }
+}
