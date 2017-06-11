aece46ceddee8a231c28093110ee27c263da1977
Eden Shuker <edenshuker1997@gmail.com>
2017-06-11 14:41:13 +0300

    updates

--- MazeGame/Controllers/SingleGameController.cs
+++ MazeGame/Controllers/SingleGameController.cs
@@ -5,10 +5,29 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using MazeGame.Models;
+using Newtonsoft.Json.Linq;
+using MazeLib;
+using SearchAlgorithmsLib;
 
 namespace MazeGame.Controllers
 {
     public class SingleGameController : ApiController
     {
+
+        [HttpPost]
+        [Route("api/SingleGame/GenerateMaze")]
+        public JObject GenerateMaze(string name, int rows, int cols)
+        {
+            Maze maze = ServerModel.GetInstance().GenerateMaze(name, rows, cols);
+            JObject obj = JObject.Parse(maze.ToJSON());
+            return obj;
+        }
+
+        //[HttpPost]
+        //[Route("api/SingleGame/SolveMaze")]        //public JObject SolveMaze(string mazeName, int algo)
+        //{
+        //    Solution<Position> solution = ServerModel.GetInstance().SolveMaze(mazeName, algo);
+            
+        //}
     }
 }
--- MazeGame/navbar.js
+++
@@ -1,7 +0,0 @@
-var navbar=" <nav class='navbar navbar-inverse'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='/HomePage.html'>Maze</a></div><div id='mazeNavbar' class='navbar-collapse collapse'><ul class='nav navbar-nav'><li><a href='/SinglePlayerPage.html'>Single Game</a></li><li><a href='#'>Multiplayer Game</a></li><li><a href='#'>Settings</a></li><li><a href='#'>User Rankings</a></li></ul><ul class='nav navbar-nav navbar-right'><li><a href='/Register.html'>Register</a></li><li><a href='/Login.html'>Login</a></li></ul></div><!--/.nav-collapse --></div><!--/.container-fluid --></nav>"
-
-
-$(function(){
-$(".navigationbar").html(navbar);
-
-});
