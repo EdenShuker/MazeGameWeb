d11ee8790c85727a65b1eea7aacfe0a108542358
Tamir <Tamir@DESKTOP-8R67EMV>
2017-06-18 00:58:51 +0300

    part pf logout still the logout.click not working

--- MazeGame/Scripts/app.js
+++ MazeGame/Scripts/app.js
@@ -13,8 +13,14 @@
             this.name1 = self.userName();
             this.password1 = self.password();
             alert(this.password1 + " " + data.Password);
-            if (this.password1 == data.Password) {
+            if (this.password1 === data.Password) {
                 sessionStorage.setItem('user', this.name1);
+                // apply logout
+                $("#btnLog").on("click", function () {
+                    sessionStorage.removeItem('user');
+                    $("#btnLog").off("click");
+                    window.location.href = "../Views/Login.html";
+                });
                 alert("passwords match");
                 window.location.href = "../index.html";
             } else {
@@ -25,25 +31,30 @@
             .fail(function (jqXHR, textStatus, err) {
                 alert("user doesn't exist");
             });
-
     };
 
 
     self.register = function () {
-        if (self.password() == self.confirm_password()) {
-            var user = {
-                Name: self.userName(),
-                Email: self.email(),
-                Password: self.password()
-            };
-            $.post(usersUri, user).done(function (item) {
-                alert(item.Name + " " + item.Email);
-            })
-                .fail(function (jqXHR, textStatus, err) {
-                    alert("adding user failed");
-                });
+        if (self.userName() === "" || self.email() === "" || self.password() === "" || self.confirm_password() === "") {
+            alert("Fill all the fields");
         } else {
-            alert("Passwords don't match, please try again.")
+            if (self.email().includes("@") && self.password() == self.confirm_password()) {
+                var user = {
+                    Name: self.userName(),
+                    Email: self.email(),
+                    Password: self.password()
+                };
+                $.post(usersUri, user).done(function (item) {
+                    alert(item.Name + " " + item.Email);
+                })
+                    .fail(function (jqXHR, textStatus, err) {
+                        alert("adding user failed");
+                    });
+            } else {
+                $("#password").val("");
+                $("#confirm_password").val("");
+                alert("Passwords don't match, please try again.");
+            }
         }
     };
 
--- MazeGame/Scripts/navbar.js
+++ MazeGame/Scripts/navbar.js
@@ -1,16 +1,37 @@
 var navbar =
-    " <nav class='navbar navbar-inverse'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='../index.html'>Maze</a></div><div id='mazeNavbar' class='navbar-collapse collapse'><ul class='nav navbar-nav'><li><a href='../Views/SinglePlayerPage.html'>Single Game</a></li><li><a href='#'>Multiplayer Game</a></li><li><a href='#'>Settings</a></li><li><a href='../Views/UserRankings.html'>User Rankings</a></li></ul><ul class='nav navbar-nav navbar-right'><li><a href='../Views/Register.html'>Register</a></li><li><a href='../Views/Login.html'>Login</a></li></ul></div><!--/.nav-collapse --></div><!--/.container-fluid --></nav>"
+    " <nav id='navbar' class='navbar navbar-inverse'>\
+        <div class='container-fluid'>\
+        <div class='navbar-header'><a class='navbar-brand' href='../index.html'>Maze</a></div>\
+        <div id='mazeNavbar' class='navbar-collapse collapse'>\
+            <ul class='nav navbar-nav'>\
+                <li><a href='../Views/SinglePlayerPage.html'>Single Game</a></li>\
+                <li><a href='../Views/MultiPlayerPage.html'>Multiplayer Game</a></li>\
+                <li><a href='#'>Settings</a></li>\
+                <li><a href='../Views/UserRankings.html'>User Rankings</a></li>\
+            </ul>\
+            <ul class='nav navbar-nav navbar-right'>\
+                <li><a href='../Views/Register.html'>Register</a></li>\
+                <li><a id='btnLog' href='../Views/Login.html'>Login</a></li>\
+            </ul>\
+        </div>\
+        <!--/.nav-collapse -->\
+        </div>\
+        \
+    <!--/.container-fluid -->\
+    </nav>";
 
-
-$(function() {
-    $(".navigationbar").html(navbar);
-});
+var login = "href='../Views/Login.html'>Login";
+var logout = "href='#'>Logout";
 
 
 $(function () {
-    if (sessionStorage.user != null) {
-        navbar =
-            " <nav class='navbar navbar-inverse'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='../index.html'>Maze</a></div><div id='mazeNavbar' class='navbar-collapse collapse'><ul class='nav navbar-nav'><li><a href='../Views/SinglePlayerPage.html'>Single Game</a></li><li><a href='#'>Multiplayer Game</a></li><li><a href='#'>Settings</a></li><li><a href='../Views/UserRankings.html'>User Rankings</a></li></ul><ul class='nav navbar-nav navbar-right'><li><a href='../Views/Register.html'>Register</a></li><li><a href='#'>Logout</a></li></ul></div><!--/.nav-collapse --></div><!--/.container-fluid --></nav>"
+    if (sessionStorage.user == null) {
+        // change text to login
+        navbar = navbar.replace(logout, login);
+        $(".navigationbar").html(navbar);
+    } else {
+        // change text to logout
+        navbar = navbar.replace(login, logout);
         $(".navigationbar").html(navbar);
     }
 });
