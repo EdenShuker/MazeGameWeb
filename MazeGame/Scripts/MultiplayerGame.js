c435a27988ac6f5a13be6a3d5e06d48c55527505
Eden Shuker <edenshuker1997@gmail.com>
2017-06-19 12:06:46 +0300

    updates

--- MazeGame/Scripts/MultiplayerGame.js
+++ MazeGame/Scripts/MultiplayerGame.js
@@ -1,4 +1,5 @@
-﻿var multiGame = $.connection.gameHub;
+﻿// Declare a proxy to reference the hub
+var multiGame = $.connection.gameHub;
 
 var competitorContext;
 var competitorImg;
@@ -12,97 +13,38 @@ multiGame.client.moveOtherPlayer = function (direction) {
 
     // change position of competitor
     switch (direction) {
-    case 37:
-        // left
-        competitorCol -= 1;
-        break;
-    case 38:
-        // up
-        competitorRow -= 1;
-        break;
-    case 39:
-        // right
-        competitorCol += 1;
-        break;
-    case 40:
-        // down
-        competitorRow += 1;
-        break;
+        case 37:
+            // left
+            competitorCol -= 1;
+            break;
+        case 38:
+            // up
+            competitorRow -= 1;
+            break;
+        case 39:
+            // right
+            competitorCol += 1;
+            break;
+        case 40:
+            // down
+            competitorRow += 1;
+            break;
     }
 
     // update on screen
     competitorContext.drawImage(competitorImg, competitorCol * cellWidth, competitorRow * cellHeight, cellWidth, cellHeight);
 };
 
-(function() {
-    $("#startGame").click(function () {
-        $.connection.start().done(function() {
-            // extract info
-            var name = $("#mazeName").val();
-            var rows = $("#rows").val();
-            var cols = $("#cols").val();
 
-            // start new game
-            var mazeJson = multiGame.StartGame(name, rows, cols);
-            // call plugin
-            var mazeData = [mazeJson["Rows"], mazeJson["Cols"], mazeJson["Maze"]];
-            var myMazeBoard = $("#myCanvas").mazeBoard(
-                mazeData,
-                mazeJson["Start"]["Row"],
-                mazeJson["Start"]["Col"],
-                mazeJson["End"]["Row"],
-                mazeJson["End"]["Col"],
-                "Images/minion.gif",
-                "Images/Exit.png",
-                true,
-                function (direction, playerRow, playerCol) {
-                    var isNewMove = false;
-                    switch (direction) {
-                        case 37:
-                            // left
-                            if (playerCol > 0 && mazeStr[playerRow * cols + playerCol - 1] !== "1") {
-                                playerCol -= 1;
-                                isNewMove = true;
-                            }
-                            break;
-                        case 38:
-                            // up
-                            if (playerRow > 0 && mazeStr[(playerRow - 1) * cols + playerCol] !== "1") {
-                                playerRow -= 1;
-                                isNewMove = true;
-                            }
-                            break;
-                        case 39:
-                            // right
-                            if (playerCol < cols - 1 && mazeStr[playerRow * cols + playerCol + 1] !== "1") {
-                                playerCol += 1;
-                                isNewMove = true;
-                            }
-                            break;
-                        case 40:
-                            // down
-                            if (playerRow < rows - 1 && mazeStr[(playerRow + 1) * cols + playerCol] !== "1") {
-                                playerRow += 1;
-                                isNewMove = true;
-                            }
-                            break;
-                    }
-                    if (isNewMove) {
-                        // if valid move --> notify
-                        multiGame.server.Play(direction);
-                    }
-                    return [playerRow, playerCol];
-                });
-        });
-    });
-    drawCompetitorBoard();
-})();
+$("#startGame").click(function () {
+    $.connection.hub.start().done(function () {
+        // extract info
+        var name = $("#mazeName").val();
+        var rows = $("#rows").val();
+        var cols = $("#cols").val();
 
-(function() {
-    $("#joinGame").click(function () {
-        // join to game
-        var name = $("#dropdown").val();
-        var mazeJson = multiGame.server.JoinTo(name);
+        // start new game
+        var mazeJson = multiGame.StartGame(name, rows, cols);
         // call plugin
         var mazeData = [mazeJson["Rows"], mazeJson["Cols"], mazeJson["Maze"]];
         var myMazeBoard = $("#myCanvas").mazeBoard(
@@ -153,10 +95,71 @@ multiGame.client.moveOtherPlayer = function (direction) {
                 return [playerRow, playerCol];
             });
     });
-    drawCompetitorBoard();
-})();
+    drawCompetitorBoard(mazeJson);
+});
 
-var drawCompetitorBoard = function(mazeJson) {
+
+
+
+$("#joinGame").click(function () {
+    // join to game
+    var name = $("#dropdown").val();
+    var mazeJson = multiGame.server.JoinTo(name);
+    // call plugin
+    var mazeData = [mazeJson["Rows"], mazeJson["Cols"], mazeJson["Maze"]];
+    var myMazeBoard = $("#myCanvas").mazeBoard(
+        mazeData,
+        mazeJson["Start"]["Row"],
+        mazeJson["Start"]["Col"],
+        mazeJson["End"]["Row"],
+        mazeJson["End"]["Col"],
+        "Images/minion.gif",
+        "Images/Exit.png",
+        true,
+        function (direction, playerRow, playerCol) {
+            var isNewMove = false;
+            switch (direction) {
+                case 37:
+                    // left
+                    if (playerCol > 0 && mazeStr[playerRow * cols + playerCol - 1] !== "1") {
+                        playerCol -= 1;
+                        isNewMove = true;
+                    }
+                    break;
+                case 38:
+                    // up
+                    if (playerRow > 0 && mazeStr[(playerRow - 1) * cols + playerCol] !== "1") {
+                        playerRow -= 1;
+                        isNewMove = true;
+                    }
+                    break;
+                case 39:
+                    // right
+                    if (playerCol < cols - 1 && mazeStr[playerRow * cols + playerCol + 1] !== "1") {
+                        playerCol += 1;
+                        isNewMove = true;
+                    }
+                    break;
+                case 40:
+                    // down
+                    if (playerRow < rows - 1 && mazeStr[(playerRow + 1) * cols + playerCol] !== "1") {
+                        playerRow += 1;
+                        isNewMove = true;
+                    }
+                    break;
+            }
+            if (isNewMove) {
+                // if valid move --> notify
+                multiGame.server.Play(direction);
+            }
+            return [playerRow, playerCol];
+        });
+    drawCompetitorBoard(mazeJson);
+});
+
+
+
+var drawCompetitorBoard = function (mazeJson) {
     // call plugin
     var mazeData = [mazeJson["Rows"], mazeJson["Cols"], mazeJson["Maze"]];
     var competitorBoard = $("#competitorCanvas").mazeBoard(
@@ -174,8 +177,8 @@ var drawCompetitorBoard = function(mazeJson) {
     cellHeight = competitorBoard[0].cellHeight;
     competitorContext = competitorBoard[0].context;
     competitorImg = competitorBoard[0].playerImg;
-}
+};
 
-multiGame.client.closeGame = function() {
+multiGame.client.closeGame = function () {
 
 };
